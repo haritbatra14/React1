dcb7dc83ebb0f1e998e00c6afaeea4e435ab786f
Harit batra <you@example.com>
2023-12-09 16:40:59 +0530

    cd

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React1 from './Components/React1';
+
+function App() {
+  return (
+    <>
+    <React1></React1>
+    </>
+  );
+}
+
+export default App;
